6dadc4b5b81e1572d06bde83a2efaad130d338b8
wddo <wddoddo@gmail.com>
2022-10-06 16:54:44 +0900

    v-model, functional'

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>I'm {{ im }}</div>
+  <div>
+    <button @click="clickHandler">
+      wddo
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      default: "",
+      required: false,
+    },
+  },
+  emits: ["update:modelValue"],
+  data() {
+    return {
+      myName: "Child",
+    };
+  },
+  computed: {
+    im() {
+      return !!this.modelValue.length ? this.modelValue : this.myName;
+    },
+  },
+  methods: {
+    clickHandler() {
+      this.$emit("update:modelValue", "wddo");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/DynamicHeading.vue
@@ -0,0 +1,22 @@
+<template>
+  <component
+    :is="`h${$props.level}`"
+    v-bind="$attrs"
+  >
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    level: {
+      type: String,
+      default: "1",
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
